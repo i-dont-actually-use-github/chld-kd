d0272e1f40caed67f13cd65864bb6299ff55917f
i-dont-actually-use-github <114365883+i-dont-actually-use-github@users.noreply.github.com>
2023-05-12 12:40:13 -0700

    Update $onInteraction[confirm2].cmd

--- $onInteraction[confirm2].cmd
+++ $onInteraction[confirm2].cmd
@@ -1,1 +1,7 @@
-
+$nomention
+$ephemeral[$onlyForIDs[1023032665785511946;You are unable to approve or deny this request, only administrators have this ability]]
+$if[$authorID==1023032665785511946]
+❌ Economy reset denied successfully. ❌
+$editButton[confirm1;Yes ✅;success;yes;]
+$editButton[confirm2;No ❌;danger;yes;]
+$endif

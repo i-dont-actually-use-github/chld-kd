959570b48ab40a0418a83ce1ccd236093208ec6d
i-dont-actually-use-github <114365883+i-dont-actually-use-github@users.noreply.github.com>
2023-05-12 12:16:25 -0700

    Create shop.cmd

---
+++ shop.cmd
@@ -0,0 +1,12 @@
+$nomention
+$if[$getUserVar[Blacklist;$authorID]==1]
+You are blacklisted from the bot.
+$else
+$title[__***CHILDREN SHOP***__]
+$description[📱 GoofyPhone K34 Pro 5G - 435,000 <:Child:1099617496018985041> Children.
+🀄 Goofy Card - 1,000,000 <:Child:1099617496018985041> Children.
+✉️ Goofy Postcard - 5,000,000 <:Child:1099617496018985041> Children.
+<a:eidwh:1099470536767184986> Ultimate Goofy Cube - 50,000,000 <:Child:1099617496018985041> Children.]
+$footer[Above is a list of the items you can purchase with <:Child:1099617496018985041> children.]
+$color[5d0992]
+$endif

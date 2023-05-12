336103b860ffdb04c7960d54a3bee6737cc08b83
i-dont-actually-use-github <114365883+i-dont-actually-use-github@users.noreply.github.com>
2023-05-12 12:14:24 -0700

    Create credits.cmd

---
+++ credits.cmd
@@ -0,0 +1,20 @@
+$nomention
+$if[$getUserVar[Blacklist;$authorID]==1]
+You are blacklisted from the bot.
+$else
+$title[__***Credits:***__]
+$description[SwagSilver#4228: Helped with code, ideas, goofy card `+buy` artwork
+
+Konek#2167: Creator, coded most of the original bot
+
+shut up#3478: Konek alt, used for testing
+
+pork#6356: Goofy Postcard idea and `+buy` artwork, goofy phone `+buy` artwork, goofy card `+use` artwork, goofy postcard `+use` artwork
+
+Ja2019HG#3993: Testing `+daily`
+
+jacket#0390: Being gay 💀]
+$footer[⚠️ Note: credits might not be entirely up-to-date. ⚠️]
+$endif
+$addButton[no;hog1;1;primary;no;]
+$addButton[no;hog;2;primary;no;]

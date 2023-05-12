df22750f978d8e403f518a75a4985eb19a18de15
i-dont-actually-use-github <114365883+i-dont-actually-use-github@users.noreply.github.com>
2023-05-12 12:26:40 -0700

    Create info.cmd

---
+++ info.cmd
@@ -0,0 +1,21 @@
+$nomention
+$if[$getUserVar[Blacklist;$authorID]==1]
+You are blacklisted from the bot.
+$else
+$title[__***Children Kidnapper***__]
+$description[
+__***Bot owner***__ : Konek#2167
+
+__***Ping***__ : $pingms (milliseconds)
+
+__***Creation Date***__ (YYYY/DD/MM): 2023-21-04
+
+__***Bot ID***__ : 1099071282131255406
+
+__***Administrator List***__ : Konek#2167,
+
+__***Server Count***__ : $serverCount
+
+*You must ask me (Konek#2167) if you want to invite the bot to your server(s).*]
+$footer[ℹ️ Basic bot info ℹ️]
+$endif

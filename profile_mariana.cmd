b24dfaa6416b59bf13f15a04b9692bae46f43e8b
i-dont-actually-use-github <114365883+i-dont-actually-use-github@users.noreply.github.com>
2023-05-12 12:33:58 -0700

    Create profile_mariana.cmd

---
+++ profile_mariana.cmd
@@ -0,0 +1,11 @@
+$nomention
+$title[__***MARIANIAN CHILDREN PROFILE***__]
+$description[__***Type***__ : Special
+__***Description***__ : Normal children from the 🇲🇵 Northern Mariana Islands.
+__***Fighting Type***__ : Prehistoric
+__***Main Weapons***__ : Axe, Rocks
+__***Rarity***__ : Rare
+ 
+♥️ HP: 6500
+🗡️ ATK: 990]
+$image[https://media.discordapp.net/attachments/1023500684215210085/1099605444198150205/Untitled46_20230423005814.png]

0032978ce8b61f47afdec7252ad4652ca5dfcd03
i-dont-actually-use-github <114365883+i-dont-actually-use-github@users.noreply.github.com>
2023-05-12 12:43:16 -0700

    Create medievalboard.cmd

---
+++ medievalboard.cmd
@@ -0,0 +1,9 @@
+$nomention
+$if[$getUserVar[Blacklist;$authorID]==1]
+You are blacklisted from the bot.
+$else
+$title[__***Medievalboard for this server***__]
+$userLeaderboard[Easter;asc]
+$footer[ℹ️ Listed above are the users with the most medievals in this server. ℹ️]
+$color[ffffff]
+$endif

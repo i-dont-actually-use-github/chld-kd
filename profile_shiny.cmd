13d80e11b5cf63be3b03d0eff72a2deb899fd120
i-dont-actually-use-github <114365883+i-dont-actually-use-github@users.noreply.github.com>
2023-05-12 12:34:44 -0700

    Create profile_shiny.cmd

---
+++ profile_shiny.cmd
@@ -0,0 +1,11 @@
+$nomention
+$title[__***SHINY CHILDREN PROFILE***__]
+$description[__***Type***__ : Normal
+__***Description***__ : Normal children, but in shiny form.
+__***Fighting Type***__ : Normal (x20 multiplied ATK)
+__***Main Weapon(s)***__ : Telekinesis
+__***Rarity***__ : Extremely Rare
+ 
+♥️ HP: 20,000
+🗡️ ATK: 5900]
+$image[https://media.discordapp.net/attachments/1023500684215210085/1099611438001172480/Untitled47_20230423012148.png]

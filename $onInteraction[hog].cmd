155bffa092e42bf375eb2a2441e4487e3f165b3f
i-dont-actually-use-github <114365883+i-dont-actually-use-github@users.noreply.github.com>
2023-05-12 12:36:27 -0700

    Create $onInteraction[hog].cmd

---
+++ $onInteraction[hog].cmd
@@ -0,0 +1,7 @@
+$nomention
+$title[__***Credits:***__]
+$description[Щещ19231+++#6570: Other Konek alt account, used for testing
+
+children eater#4671: Original bot, made by me (Konek)]
+$footer[⚠️ Note: credits might not be entirely up-to-date. ⚠️]
+$endif

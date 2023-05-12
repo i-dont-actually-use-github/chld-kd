2d1239a3dd7a0276a698f8d53cb7eaa8a6fbc487
i-dont-actually-use-github <114365883+i-dont-actually-use-github@users.noreply.github.com>
2023-05-12 12:36:51 -0700

    Create $onInteraction[hog1].cmd

---
+++ $onInteraction[hog1].cmd
@@ -0,0 +1,14 @@
+$nomention
+$title[__***Credits:***__]
+$description[SwagSilver#4228: Helped with code, ideas, goofy card `+buy` artwork
+
+Konek#2167: Creator, coded most of the original bot
+
+shut up#3478: Konek alt, used for testing
+
+pork#6356: Goofy Postcard idea and `+buy` artwork, goofy phone `+buy` artwork, goofy card `+use` artwork, goofy postcard `+use` artwork
+
+Ja2019HG#3993: Testing `+daily`
+
+jacket#0390: Being gay 💀]
+$footer[⚠️ Note: credits might not be entirely up-to-date. ⚠️]

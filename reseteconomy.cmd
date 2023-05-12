8f4bca9391da5c534182fd6b18fed5798271bbf0
i-dont-actually-use-github <114365883+i-dont-actually-use-github@users.noreply.github.com>
2023-05-12 12:37:29 -0700

    Create reseteconomy.cmd

---
+++ reseteconomy.cmd
@@ -0,0 +1,5 @@
+$nomention
+$onlyForIDs[1023032665785511946; Only administrators can run this command.]
+Are you sure you wish to reset this server's economy?
+$addButton[no;confirm1;Yes ✅;success;no;]
+$addButton[no;confirm2;No ❌;danger;no;]

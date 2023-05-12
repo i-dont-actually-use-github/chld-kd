1a9ac2a00bc748706d30749d1ec3f28cbe38d454
i-dont-actually-use-github <114365883+i-dont-actually-use-github@users.noreply.github.com>
2023-05-12 12:37:07 -0700

    Create marianaboard.cmd

---
+++ marianaboard.cmd
@@ -0,0 +1,9 @@
+$nomention
+$if[$getUserVar[Blacklist;$authorID]==1]
+You are blacklisted from the bot.
+$else
+$title[__***Marianaboard for this server***__]
+$userLeaderboard[Mariana;asc]
+$footer[ℹ️ Listed above are the users with the most Marianian children in this server. ℹ️]
+$color[0030a2]
+$endif
